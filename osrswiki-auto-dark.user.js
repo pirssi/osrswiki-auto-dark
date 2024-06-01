faf00409768e352af48786c9b3597b34c317ab2a
pirssi <49979169+pirssi@users.noreply.github.com>
2024-06-02 01:22:57 +0300

    first commit

---
+++ osrswiki-auto-dark.user.js
@@ -0,0 +1,97 @@
+// ==UserScript==
+// @name        OSRS Wiki Auto Dark Mode
+// @description Automatic Dark Mode switcher for Old School Runescape Wiki
+// @version     1
+// @author      pirssi
+// @namespace   https://pirss.in
+// @match       *://oldschool.runescape.wiki/*
+// @grant       none
+// @license     MIT
+// @homepageURL https://github.com/pirssi/osrswiki-auto-dark/
+// @supportURL  https://github.com/pirssi/osrswiki-auto-dark/
+// @downloadURL https://github.com/pirssi/osrswiki-auto-dark/raw/main/osrswiki-auto-dark.user.js
+// @updateURL   https://github.com/pirssi/osrswiki-auto-dark/raw/main/osrswiki-auto-dark.user.js
+// ==/UserScript==
+
+/*
+Automatically switches Old School RuneScape (OSRS) Wiki dark mode according to user's browser/system theme preference.
+
+Enables the built-in dark mode by changing CSS classes on the page according to browser theme ("prefers-color-scheme").
+
+To use, turn on dark mode on the Wiki from the menu on the top of the page, and the site will follow your browser's light/dark mode.
+
+Additionally, you can enable and set a custom time frame in which the script will change to dark mode. E.g. you want to use the light theme on the Wiki while using dark mode in your browser during the day. See the below for how to set the time frame and enable it.
+*/
+
+// CUSTOM TIME FRAME
+
+// whether to use custom time frame in which to follow the user's system theme prefence
+// true = use a custom time frame (set below)
+// false = always follow system theme preference
+const USE_TIMEFRAME = false;
+
+// set the time here, 24-hour time format with no leading zeroes. ignored if USE_TIMEFRAME is set to false.
+// (e.g. enable following theme prefence when system clock is 07:01 pm: START_HOUR = 19 & START_MINUTES = 1)
+const START_HOUR = 19;
+const START_MINUTES = 0;
+const END_HOUR = 7;
+const END_MINUTES = 0;
+
+// ---
+
+// function to check if current time is between the start time and the end time when USE_TIMEFRAME is set to true
+function checkTime() {
+  if (!USE_TIMEFRAME) {
+    return true;
+  } else {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const currentTime = hours * 60 + minutes; // current time (minutes since midnight)
+    const startTime = START_HOUR * 60 + START_MINUTES;
+    const endTime = END_HOUR * 60 + END_MINUTES;
+    return (
+      (startTime < endTime && startTime < currentTime && currentTime < endTime) ||
+      (startTime > endTime && startTime < currentTime && currentTime > endTime) ||
+      (startTime > endTime && startTime > currentTime && currentTime < endTime)
+    );
+  }
+}
+
+// function to set theme based on system preference and time
+function setTheme() {
+  if (window.matchMedia) {
+    const systemIsDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const useDarkMode = systemIsDark && checkTime();
+    if (useDarkMode) {
+      document.body.classList.remove("wgl-lightmode");
+      document.body.classList.remove("wgl-theme-light");
+      document.body.classList.add("wgl-darkmode");
+      document.body.classList.add("wgl-theme-dark");
+    } else {
+      document.body.classList.remove("wgl-darkmode");
+      document.body.classList.remove("wgl-theme-dark");
+      document.body.classList.add("wgl-lightmode");
+      document.body.classList.add("wgl-theme-light");
+    }
+  }
+}
+
+// set initial theme on load
+setTheme();
+
+// update theme once a minute
+setInterval(setTheme, 60000);
+
+// add listener for system theme changes
+if (window.matchMedia) {
+  const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  darkModeQuery.addEventListener("change", setTheme);
+}
+
+// update when the page becomes visible again
+document.addEventListener("visibilitychange", function () {
+  if (!document.hidden) {
+    setTheme();
+  }
+});
